b0eae481eb27e7aee7419119383f2d0cb2dad9ef
Kaname1013 <leeavril1013@gmail.com>
2025-03-06 15:02:34 +0800

    2025/3/6

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace KANAME
+{
+    public class Practice : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log(message: "Awake");
+        }
+
+        private void Start()
+        {
+            Debug.Log(message: $"Start");
+
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log(message: $"OnEnble");
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log(message: $"OnDisable");
+        }
+        private void OnDestroy()
+        {
+            Debug.Log(message: $"OnDestroy");
+        }
+    }
+}
